cfef83278793144378cf83218263f0c8d3d47e94
Nathan Guimarães <nathangrs8@gmail.com>
2021-12-20 11:28:39 -0300

    Criacao do slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,15 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*============== QUESTIONS ACCORDION ==================*/
+
+/*============== TESTIMONIAL SWIPER ==================*/
+
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop:'true',
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
