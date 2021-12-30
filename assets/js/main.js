9bf81a7bf71e9fb5da6a0c0cc946c5c8c9ac0394
Nathan Guimarães <nathangrs8@gmail.com>
2021-12-30 17:53:42 -0300

    Conclusao do slide e finalizacao do formulario de email

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,23 @@ let testimonialSwiper = new Swiper(".testimonial-swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  /*============== NEW SWIPER ==================*/
+
+  let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop:'true',
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    }
+    
+  });
