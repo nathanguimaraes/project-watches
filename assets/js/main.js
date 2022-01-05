34ebd849790ba088adf186e4f7cd453b653762d6
Nathan Guimarães <nathangrs8@gmail.com>
2022-01-05 16:29:40 -0300

    finalizacao do carrinho

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,27 @@ function scrollUp(){
     if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+
+/*=========================================================================== */
+
+/*=============== SHOW CART===============*/
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cart.classList.remove('show-cart')
+    })
+}
